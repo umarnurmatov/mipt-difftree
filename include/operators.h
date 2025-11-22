6b8dafdd8418379ebed1610560fef5941501b11b
umarnurmatov <umarkhonnur12@gmail.com>
2025-11-22 11:48:25 +0300

    #ADDED latex_str to Operator

--- include/operators.h
+++ include/operators.h
@@ -7,19 +7,20 @@ typedef struct Operator
 {
     OperatorType type;
     const char* str;
+    const char* latex_str;
     utils_hash_t hash;
-} 
-Operator;
 
-#define MAKE_OPERATOR(str, type) \
-    { type, str, utils_djb2_hash(str, SIZEOF(str)) }
+} Operator;
+
+#define MAKE_OPERATOR(str, latex_str, type) \
+    { type, str, latex_str, utils_djb2_hash(str, SIZEOF(str)) }
 
 static Operator op_arr[] = 
 {
-    MAKE_OPERATOR("+", OPERATOR_TYPE_ADD),
-    MAKE_OPERATOR("-", OPERATOR_TYPE_SUB),
-    MAKE_OPERATOR("*", OPERATOR_TYPE_MUL),
-    MAKE_OPERATOR("/", OPERATOR_TYPE_DIV)
+    MAKE_OPERATOR("+", "+",      OPERATOR_TYPE_ADD),
+    MAKE_OPERATOR("-", "-",      OPERATOR_TYPE_SUB),
+    MAKE_OPERATOR("*", "\\cdot", OPERATOR_TYPE_MUL),
+    MAKE_OPERATOR("/", "/",      OPERATOR_TYPE_DIV)
 };
 
 // int operators_prepare_op_arr();
